3d90db3a2dc97541de8dad0d226ba8366384ff81
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-04-03 04:19:36 +0900

    Section 2-3 : API

--- jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,31 @@ public class HelloController {
     public String helloMvc(@RequestParam(name = "name") String name, Model model) { // default : required = true, "name" 뒤에 ',' 쓰고 씀. 생략 가능
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 통신 프로토콜에서의 헤더/바디 부분에 이 데이터를 직접 넣음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
