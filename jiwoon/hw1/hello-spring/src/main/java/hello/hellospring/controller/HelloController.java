45e09833e783565afb8f2a0181cdf9cf5bdc1758
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-04-03 03:55:40 +0900

    Section 2-2 : MVC와 템플릿 엔진

--- jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,7 +11,12 @@ public class HelloController {
     @GetMapping
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model) { // default : required = true, "name" 뒤에 ',' 쓰고 씀. 생략 가능
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
