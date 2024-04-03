b691d239c27b03cc718b3f3d4307e943b1ab59a2
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-04-04 02:34:35 +0900

    Section 4-1 : 컴포넌트 스캔과 자동 의존관계 설정

--- jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ jiwoon/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService { // ctrl + shift + T => 테스트 생성 단축키
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository; // 직접 new하지 않고 외부에서 넣어줌 -> Dependency Injection(DI)
     }
