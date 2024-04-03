b71d882330d85571a7b5b544f43ec8115e9046a1
rtgc0915 <rtgc0915@gmail.com>
2024-04-03 23:25:58 +0900

    Section 3-4: 회원 서비스 개발
    Feat: MemberService : 회원 repository와 Domain을 활용해서 실제 비즈니스 로직을 작성하는 class

---
+++ seungbin/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//service 는 비즈니스 롤에 맞도록 네이밍
+public class MemberService {
+
+    private final MemberRepository memberRepository  = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        //findByName을 통해 로직이 쭉나오면 -> 메소드로 뽑는게 나음(ctrl+T(Extract Method))
+        validateDuplicateMember(member); //중복 회원 X
+//        memberRepository.findByName(member.getName())
+//                .ifPresent(m -> {
+//                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+//                });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
