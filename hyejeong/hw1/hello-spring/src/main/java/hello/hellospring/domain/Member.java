e9c32b37167245cb23ddf094eca9c079e50fdd4a
HyeJeong <rucy011121@gmail.com>
2024-04-04 12:48:20 +0900

    Section 3-2 : 회원 도메인과 리포지토리 만들기

---
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id; //고객이 정하는 게 아닌, 시스템이 정하는 id
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
