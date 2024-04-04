4eff44fb8c784ad4bde0c93127fe86f9b6c49609
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-04-04 20:52:19 +0900

    Section 6-2 : 순수 JDBC

--- jiwoon/hw1/hello-spring/build.gradle.kts
+++ jiwoon/hw1/hello-spring/build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation ("org.springframework.boot:spring-boot-starter-jdbc") // 자바는 DB와 붙으려면 기본적으로 jdbc 드라이버가 꼭 필요
+	runtimeOnly ("com.h2database:h2") // DB랑 붙을때 데이터베이스가 제공하는 클라이언트 필요. 여기에서는 h2 데이터베이스 클라이언트 필요
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
