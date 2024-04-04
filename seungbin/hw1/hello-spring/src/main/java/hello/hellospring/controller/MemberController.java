6cb557f47c1ebf9243b1dffe3359991743dbf6ef
rtgc0915 <rtgc0915@gmail.com>
2024-04-04 10:57:52 +0900

    Section 5-3: 회원 웹 기능 - 조회
    Feat: memberList template 생성
    MemberController get, memberList 조회

--- seungbin/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ seungbin/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 //생성해두면 스프링 컨테이너에서 스피링 빈이 관리됨
 //스프링 빈을 등록할 때는 싱글톤으로만 등록됨
@@ -32,5 +35,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
