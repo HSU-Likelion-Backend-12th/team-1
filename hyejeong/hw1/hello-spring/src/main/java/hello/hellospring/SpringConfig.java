c1cb6d60c6e881613863657a6393830d449d4106
HyeJeong <rucy011121@gmail.com>
2024-04-04 23:10:50 +0900

    Section 6-4 : 스프링 JdbcTemplate

--- hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
     //MemberService와 MemberRepository를 둘다 스프링 빈에 등록
     //스프링 빈에 등록되어 있는 MemberRepository를 MemberService에 넣어줌
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
