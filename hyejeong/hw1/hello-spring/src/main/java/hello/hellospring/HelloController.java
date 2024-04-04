b43e7e1d478407baad7d84828f0c22e16fd447c3
HyeJeong <rucy011121@gmail.com>
2024-04-04 12:12:07 +0900

    Section 2-3 : API

--- hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/HelloController.java
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller /*컨트롤러 annotation*/
 public class HelloController {
@@ -18,5 +19,32 @@ public class HelloController {
     public String hello(Model model) { //model : model view controller
         model.addAttribute( "data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //html에 나오는 body가 아니라 http 통신 프로토콜의 body를 뜻함
+    // ResponseBody -> 응답 body부에 이 내용을 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //"hello spring" 문자가 요청한 클라이언트에 그대로 내려간다
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello =new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
