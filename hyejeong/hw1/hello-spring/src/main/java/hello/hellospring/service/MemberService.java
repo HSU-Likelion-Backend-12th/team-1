057fab49f6d2e4872b0a7b9eef992b69b5600ceb
HyeJeong <rucy011121@gmail.com>
2024-04-04 16:06:33 +0900

    Section 3-4 : 회원 서비스 개발

--- hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,7 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemeberRepository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 
 public class MemberService {
-    private final MemeberRepository memeberRepository= new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        //외부에서 메모리 멤버 리포지토리를 넣어준다-> Dependency Injection
+        this.memberRepository=memberRepository;
+    }
 
     /**
      * 회원가입
@@ -17,12 +22,12 @@ public class MemberService {
     public Long join(Member member){
         //같은 이름이 있는 중복 회원 불가
         validateDuplicateMember(member); //중복 회원 검증
-        memeberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memeberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(m -> { //result가 깂이 있으면. 멤버 m이 들어올 것. 여기에 멤버의 값이 있으면
                     throw new IllegalStateException("이미 존재하는 회원입니다."); //null이 아니면 어떤 값이 있으면 로직 동작
                 });
@@ -32,11 +37,11 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memeberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
-        return memeberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 
 
