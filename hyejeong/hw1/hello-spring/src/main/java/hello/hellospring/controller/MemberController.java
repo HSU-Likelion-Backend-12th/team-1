1ac5b9be9437166874f1ace9401ec3e8311c686a
HyeJeong <rucy011121@gmail.com>
2024-04-04 21:33:48 +0900

    Section 5-3 : 회원 웹 기능 - 조회

--- hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,11 +1,15 @@
 package hello.hellospring.controller;
 
+
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //controller라는 애노테이션을 보고 스프링이 뜰 때
 public class MemberController {
@@ -26,12 +30,17 @@ public class MemberController {
     //데이터를 등록할 때는 post, 조회할 때는 get
     @PostMapping(value = "/members/new")
     public String create(MemberForm form){
-        Member member=new Member();
+        Member member = new Member();
         member.setName(form.getName());
-
         memberService.join(member);
-
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 
--- hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
+++ hyejeong/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -1,12 +1,10 @@
 package hello.hellospring.controller;
 
 public class MemberForm {
-    private String name; //매칭이 되면서 값이 들어옴
-
-    public String getName(){
+    private String name;
+    public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
