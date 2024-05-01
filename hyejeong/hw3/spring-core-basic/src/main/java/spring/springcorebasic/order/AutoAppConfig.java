ba04951d10392aae9847a5b3bec6f8c2c52b9de5
HyeJeong <rucy011121@gmail.com>
2024-05-02 00:58:32 +0900

    :sparkles: 3-1 : AutoAppConfig.java 생성

---
+++ hyejeong/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package spring.springcorebasic.order;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan( //Conponentscan -> @Configuration이 붙은 설정 정보 자동으로 등록
+        //앞에서 만들어두었던 설정 정보도 등록되고 실행되어 버리기 때문에
+        //설정 정보는 컴포넌트 스캔 대상에서 제외
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,
+                classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
