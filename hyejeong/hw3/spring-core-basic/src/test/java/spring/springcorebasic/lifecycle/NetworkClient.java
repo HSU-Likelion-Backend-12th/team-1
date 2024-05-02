c83cb25775a2399f0e54a9d6c27b66d5569a61ee
HyeJeong <rucy011121@gmail.com>
2024-05-02 13:58:58 +0900

    :sparkles: 3-3 : @PosetConstruct, @PreDestroy 애노테이션 작성

--- hyejeong/hw3/spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
+++ hyejeong/hw3/spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package spring.springcorebasic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = "+url);
-        connect();
-        call("초기화 연결 메시지");
 
     }
 
@@ -28,5 +29,19 @@ public class NetworkClient {
         System.out.println("close: "+url);
     }
 
+    //빈 등록을 초기화 메소드
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //빈 등록 소멸 메소드
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
 
 }
