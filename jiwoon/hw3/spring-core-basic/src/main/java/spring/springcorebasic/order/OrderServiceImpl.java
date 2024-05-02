843f593b8d3de355b4a1bbdb8080aefd356f2a5a
Hjwoon <hjw030705@naver.com>
2024-05-02 14:36:43 +0900

    3-2. 의존관계 주입

--- jiwoon/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ jiwoon/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package spring.springcorebasic.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.springcorebasic.discount.DiscountPolicy;
@@ -9,6 +10,7 @@ import spring.springcorebasic.member.MemberRepository;
 import spring.springcorebasic.member.MemoryMemberRepository;
 
 @Component
+@RequiredArgsConstructor // 생성자 코드를 그대로 만들어주기 때문에 지워줘야함
 public class OrderServiceImpl implements OrderService{
 
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
@@ -17,10 +19,10 @@ public class OrderServiceImpl implements OrderService{
 
     // 생성자 주입: 생성자를 이용하여 의존관계 주입
     //@Autowired // 생성자가 1개만 있으면 autowired 지울 수 있음
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // new를 사용하지 않고 생성자의 매개변수로 객체 주입
+    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // new를 사용하지 않고 생성자의 매개변수로 객체 주입
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
